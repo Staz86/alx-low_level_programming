2af2c1bb718a8be7741a3f9835cbe2148bbe3bce
Charles Diale <charles.diale13@gmail.com>
2023-02-16 15:57:11 -0800

    puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
-int main()
+/*prints string to stdout using puts func*/
+int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	/*prints string to stdout*/
 	puts(Mystr);
-
-	return 0;
+	return (0);
 }
