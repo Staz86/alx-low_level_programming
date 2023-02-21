7f53121860013c65ee1f3c4b49a43c832b0603da
Charles Diale <charles.diale13@gmail.com>
2023-02-21 11:54:11 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ */
+	void print_alphabet(void)
+	{
+		int x = 'a';
+
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+
+	}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-
+/* print_alphabet - prints the alphabet in lowercase*/
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
