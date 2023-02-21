e8d59148e8175fe5a8cfb75a06587225a1495c0d
Charles Diale <charles.diale13@gmail.com>
2023-02-21 15:04:56 -0800

    printSign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
+		_putchar('\n');
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
+		_putchar('\n');
 	}
 	else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
+		_putchar('\n');
 	}
 }
