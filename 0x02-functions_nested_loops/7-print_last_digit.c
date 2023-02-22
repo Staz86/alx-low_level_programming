222c4f8bc12856c6aa26255654080ac587d31f47
Charles Diale <charles.diale13@gmail.com>
2023-02-22 05:38:01 -0800

    lastDigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - finds and prints last digit of a number
+ *
+ * @c: The number to be checked
+ *
+ * Return: Last digit from int
+ */
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
