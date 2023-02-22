6c8c2360d4e96f0ea44aad57fe2b347528d1110e
Charles Diale <charles.diale13@gmail.com>
2023-02-22 06:36:21 -0800

    LastDigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,10 @@ int print_last_digit(int c)
 	int lastDigit;
 
 	lastDigit = c % 10;
+	if (lastDigit < 0)
+	{
+		lastDigit = lastDigit * -1;
+	}
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
