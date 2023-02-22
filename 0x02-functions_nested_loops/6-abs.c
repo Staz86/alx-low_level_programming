0e504bdba83402c96425acf285b4520954ba429c
Charles Diale <charles.diale13@gmail.com>
2023-02-22 01:50:31 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <math.h>
+#include <ctype.h>
+/**
+ * _abs - computes the absolute value of an int
+ *
+ * @c: Number to check
+ *
+ * Return: 1 success
+ */
+int _abs(int c)
+{
+
+	c = _abs(c);
+	return (1);
+}
