c1388ed56819cfc1ae39f4888c2f06320d2546a3
Charles Diale <charles.diale13@gmail.com>
2023-02-24 21:59:54 -0800

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,25 +10,25 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
-		}
-		else if (num % 3 == 0 && num % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (num == 100)
-		{
-			printf("%d", num);
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", num);
+		}
+		if (num != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
