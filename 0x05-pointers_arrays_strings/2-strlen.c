d9c675323cc076a388fb260732fff252e4df3efc
Charles Diale <charles.diale13@gmail.com>
2023-02-28 13:15:46 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen - returns length of a string
+* @s: pointer
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
