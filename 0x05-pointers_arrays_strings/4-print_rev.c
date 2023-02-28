f79cc01ac87c965e17a71dfcad1470af6d7f92f7
Charles Diale <charles.diale13@gmail.com>
2023-02-28 13:57:43 -0800

    printRev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: pointer
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
