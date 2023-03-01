bf719a9c3b7818a98bf8d086fab931b7c0fe10de
Charles Diale <charles.diale13@gmail.com>
2023-02-28 16:43:50 -0800

    putsHalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int start_index = len/ 2;
+
+	while (str[len] != '\0')
+	{
+		len++;
+
+
+	if (len % 2 == 1)
+	{
+		start_index++;
+	}
+	}
+	while (str[start_index] != '\0')
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+	_putchar('\n');
+}
