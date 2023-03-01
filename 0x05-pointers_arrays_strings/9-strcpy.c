ad42ed9f1c590d1b314e96d8bd6be51e5d0bb214
Charles Diale <charles.diale13@gmail.com>
2023-03-01 00:21:05 -0800

    stcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed by src
+ * @dest: points to buffer
+ * @src: points to string
+ *
+ * Return: *dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
