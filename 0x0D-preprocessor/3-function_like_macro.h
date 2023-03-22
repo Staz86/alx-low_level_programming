5fee481f6d6a73d6b6a561c2a2ba1b84db106d9d
Charles Diale <charles.diale13@gmail.com>
2023-03-22 14:13:13 -0700

    function-like macro calculating absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#include <stdio.h>
-/**
- * main - Entry point
- * @x: int argument
- * Return: x (Success)
- */
+
 #define ABS(x) (((x) < 0) ? ((-x)) : (x))
 
 #endif
