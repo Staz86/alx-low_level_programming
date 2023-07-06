1a7e002481fe30849ceda9e8a5b9f22d541eec54
Staz86 <charles.diale13@gmail.com>
2023-07-06 17:39:52 +0200

    [Fix]Task 3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask= 0;
+	unsigned long int mask = 0;
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
